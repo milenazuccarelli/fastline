b3296a461075135250e682d5ca5b0d22321d5d00
a-barbieri <info@alessandrobarbieri.net>
2020-02-11 22:13:42 +0100

    feat: add babel configuration for Jest

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+// @see https://jestjs.io/docs/en/getting-started#using-babel
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current"
+        }
+      }
+    ]
+  ]
+};
