3f7cf1e74b3ed687c30c463b3bc5a1ed7b212370
a-barbieri <info@alessandrobarbieri.net>
2020-02-12 16:51:38 +0100

    chore: copy file only if it's not present in dest dir

--- __tests__/lib/initialise.test.js
+++ __tests__/lib/initialise.test.js
@@ -3,8 +3,10 @@ import path from "path";
 import rimraf from "rimraf";
 import { uuid } from "uuidv4";
 import { setup } from "../../src/lib/initalise";
+import { getRandomInt } from "../../src/utils/common-functions";
 
 const DIR_BASE = path.resolve(__dirname, "../../__fixtures__");
+const TEMPLATES_DIR = path.resolve(__dirname, "../../templates");
 
 afterAll(done => {
   rimraf(`${DIR_BASE}/initialise-*`, () => {
@@ -13,21 +15,69 @@ afterAll(done => {
 });
 
 describe("Initialise", () => {
+  it("has some templetes files to copy", async () => {
+    let filesList;
+    await fs.promises.readdir(TEMPLATES_DIR).then(files => {
+      filesList = files;
+    });
+    expect(filesList.length).toBeGreaterThan(0);
+  });
+
   describe("when there is no setup", () => {
-    it("copies all templates", async () => {
-      const filePath = "hello-world.md";
-      const DIR_ID = uuid();
-      const DIR = path.resolve(DIR_BASE, `initialise-${DIR_ID}`);
+    it("copies at least a template", async () => {
+      let filesList, DIR_ID, DIR;
+      DIR_ID = uuid();
+      DIR = path.resolve(DIR_BASE, `initialise-${DIR_ID}`);
+      await fs.promises.readdir(TEMPLATES_DIR).then(files => {
+        filesList = files;
+      });
       await setup(DIR);
-      // return expect(fs.promises.access(filePath, fs.constants.F_OK)).rejects.toThrowError()
-      // return expect(fs.promises.access(filePath, fs.constants.F_OK)).resolves.toBeUndefined();
-      return expect(
-        fs.promises.stat(`${DIR}/${filePath}`)
-      ).resolves.toBeInstanceOf(fs.Stats);
+      const randomIndex = getRandomInt(filesList.length);
+      fs.stat(`${DIR}/${filesList[randomIndex]}`, (err, stat) => {
+        expect(stat).toBeInstanceOf(fs.Stats);
+      });
+    });
+
+    it("copies all templates", async () => {
+      let targetFilesList, originalFilesList, DIR_ID, DIR;
+      DIR_ID = uuid();
+      DIR = path.resolve(DIR_BASE, `initialise-${DIR_ID}`);
+      await fs.promises.readdir(TEMPLATES_DIR).then(files => {
+        originalFilesList = files;
+      });
+      await setup(DIR);
+      await fs.promises.readdir(`${DIR}`).then(files => {
+        targetFilesList = files;
+      });
+      expect(targetFilesList.length).toEqual(originalFilesList.length);
     });
   });
+
   describe("when a setup already exists", () => {
+    it("doesn't copy the existing templates", async () => {
+      let targetFilesList, originalFilesList, DIR_ID, DIR;
+      DIR_ID = uuid();
+      DIR = path.resolve(DIR_BASE, `initialise-${DIR_ID}`);
+      await fs.promises.readdir(TEMPLATES_DIR).then(files => {
+        originalFilesList = files;
+      });
+      const randomIndex = getRandomInt(originalFilesList.length);
+      // Create a fake file in the following order: content, directory and file.
+      const fakeContent = `This is some fake content: ${DIR_ID}`;
+      await fs.promises.mkdir(DIR, { recursive: true });
+      const fakeExistingFile = fs.createWriteStream(
+        `${DIR}/${originalFilesList[randomIndex]}`
+      );
+      fakeExistingFile.write(fakeContent);
+      fakeExistingFile.end();
+      await setup(DIR);
+      return fs.promises
+        .readFile(`${DIR}/${originalFilesList[randomIndex]}`, "utf8")
+        .then(data => {
+          expect(data).toEqual(fakeContent);
+        });
+    });
+
     it.skip("copies all templates", () => {});
-    it.skip("doesn't copy the template", () => {});
   });
 });
--- src/lib/initalise.js
+++ src/lib/initalise.js
@@ -9,10 +9,20 @@ import path from "path";
  **/
 export async function setup(targetDir) {
   const templatesDir = path.resolve(__dirname, "../../templates");
+  const fileName = "hello-world.md";
   await fs.promises.mkdir(targetDir, { recursive: true });
-  return fs.promises.copyFile(
-    `${templatesDir}/hello-world.md`,
-    `${targetDir}/hello-world.md`,
-    fs.constants.COPYFILE_EXCL
-  );
+  return fs.promises
+    .copyFile(
+      `${templatesDir}/${fileName}`,
+      `${targetDir}/${fileName}`,
+      fs.constants.COPYFILE_EXCL
+    )
+    .then(() => {
+      console.log(`${fileName} has been copied to ${targetDir}.`);
+    })
+    .catch(err => {
+      console.log(
+        `Could not copy ${fileName}. Probably the file is already present in the destination directory.`
+      );
+    });
 }
