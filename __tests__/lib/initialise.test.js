fcceab32ce748fe93c1a2d51f5adfd1df75525c1
a-barbieri <info@alessandrobarbieri.net>
2020-02-12 17:03:44 +0100

    chore: complete test for copy templates when some exists already

--- __tests__/lib/initialise.test.js
+++ __tests__/lib/initialise.test.js
@@ -8,6 +8,14 @@ import { getRandomInt } from "../../src/utils/common-functions";
 const DIR_BASE = path.resolve(__dirname, "../../__fixtures__");
 const TEMPLATES_DIR = path.resolve(__dirname, "../../templates");
 
+let filesList,
+  DIR_ID,
+  DIR,
+  fakeContent,
+  originalFilesList,
+  randomIndex,
+  targetFilesList;
+
 afterAll(done => {
   rimraf(`${DIR_BASE}/initialise-*`, () => {
     done();
@@ -15,8 +23,7 @@ afterAll(done => {
 });
 
 describe("Initialise", () => {
-  it("has some templetes files to copy", async () => {
-    let filesList;
+  it("has some templates files to copy", async () => {
     await fs.promises.readdir(TEMPLATES_DIR).then(files => {
       filesList = files;
     });
@@ -24,14 +31,16 @@ describe("Initialise", () => {
   });
 
   describe("when there is no setup", () => {
-    it("copies at least a template", async () => {
-      let filesList, DIR_ID, DIR;
+    beforeEach(async () => {
       DIR_ID = uuid();
       DIR = path.resolve(DIR_BASE, `initialise-${DIR_ID}`);
       await fs.promises.readdir(TEMPLATES_DIR).then(files => {
         filesList = files;
       });
       await setup(DIR);
+    });
+
+    it("copies at least a template", async () => {
       const randomIndex = getRandomInt(filesList.length);
       fs.stat(`${DIR}/${filesList[randomIndex]}`, (err, stat) => {
         expect(stat).toBeInstanceOf(fs.Stats);
@@ -39,7 +48,6 @@ describe("Initialise", () => {
     });
 
     it("copies all templates", async () => {
-      let targetFilesList, originalFilesList, DIR_ID, DIR;
       DIR_ID = uuid();
       DIR = path.resolve(DIR_BASE, `initialise-${DIR_ID}`);
       await fs.promises.readdir(TEMPLATES_DIR).then(files => {
@@ -54,16 +62,15 @@ describe("Initialise", () => {
   });
 
   describe("when a setup already exists", () => {
-    it("doesn't copy the existing templates", async () => {
-      let targetFilesList, originalFilesList, DIR_ID, DIR;
+    beforeEach(async () => {
       DIR_ID = uuid();
       DIR = path.resolve(DIR_BASE, `initialise-${DIR_ID}`);
       await fs.promises.readdir(TEMPLATES_DIR).then(files => {
         originalFilesList = files;
       });
-      const randomIndex = getRandomInt(originalFilesList.length);
+      randomIndex = getRandomInt(originalFilesList.length);
       // Create a fake file in the following order: content, directory and file.
-      const fakeContent = `This is some fake content: ${DIR_ID}`;
+      fakeContent = `This is some fake content: ${DIR_ID}`;
       await fs.promises.mkdir(DIR, { recursive: true });
       const fakeExistingFile = fs.createWriteStream(
         `${DIR}/${originalFilesList[randomIndex]}`
@@ -71,6 +78,9 @@ describe("Initialise", () => {
       fakeExistingFile.write(fakeContent);
       fakeExistingFile.end();
       await setup(DIR);
+    });
+
+    it("doesn't copy the existing templates", async () => {
       return fs.promises
         .readFile(`${DIR}/${originalFilesList[randomIndex]}`, "utf8")
         .then(data => {
@@ -78,6 +88,11 @@ describe("Initialise", () => {
         });
     });
 
-    it.skip("copies all templates", () => {});
+    it("copies all templates", async () => {
+      await fs.promises.readdir(`${DIR}`).then(files => {
+        targetFilesList = files;
+      });
+      expect(targetFilesList.length).toEqual(originalFilesList.length);
+    });
   });
 });
