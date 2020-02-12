da368f1a6a975b45d055068f113dde1b7aaaa468
a-barbieri <info@alessandrobarbieri.net>
2020-02-12 13:47:22 +0100

    chore: clean code

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,18 @@
 // @see https://jestjs.io/docs/en/getting-started#using-babel
 module.exports = {
-  plugins: [["@babel/plugin-transform-runtime", { corejs: 3 }]],
+  plugins: [
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        corejs: 3
+      }
+    ]
+  ],
   presets: [
     [
       "@babel/preset-env",
       {
-        useBuiltIns: "entry",
+        useBuiltIns: "usage",
         corejs: 3,
         targets: {
           node: "current"
