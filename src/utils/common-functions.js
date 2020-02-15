3b579fa9dd80a4cb9e283ef692176a6d8c341ff0
a-barbieri <info@alessandrobarbieri.net>
2020-02-15 18:02:42 +0100

    chore: add asyncForEach function

--- src/utils/common-functions.js
+++ src/utils/common-functions.js
@@ -1,3 +1,36 @@
+/**
+ * GET RANDOM INT
+ *
+ * @param  max {Number}
+ * @return {Number}
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ **/
 export function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
+}
+
+/**
+ * ASYNC FOR EACH
+ *
+ * @param array {Array}
+ * @param callback {Function}
+ *
+ * @example
+ *
+ *   const start = async () => {
+ *     await asyncForEach([1, 2, 3], async (num) => {
+ *       await waitFor(50);
+ *       console.log(num);
+ *     });
+ *     console.log('Done');
+ *   }
+ *   start();
+ *
+ * @see https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
+ **/
+export async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
 }
