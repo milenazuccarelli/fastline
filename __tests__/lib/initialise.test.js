ff09327bbadb0c04640b405d6e18e92df1abea75
a-barbieri <info@alessandrobarbieri.net>
2020-02-15 18:48:12 +0100

    chore: clean code and prepare for new tests

--- __tests__/lib/initialise.test.js
+++ __tests__/lib/initialise.test.js
@@ -2,23 +2,18 @@ import fse from "fs-extra";
 import path from "path";
 import { uuid } from "uuidv4";
 import { setup } from "../../src/lib/initalise";
-import { asyncForEach, getRandomInt } from "../../src/utils/common-functions";
+import {
+  asyncForEach,
+  getRandomInt,
+  walk
+} from "../../src/utils/common-functions";
 import { FL } from "../../src/config/init";
-import directoryTree from "directory-tree";
-import _ from "lodash";
 
 const DIR_BASE = path.resolve(__dirname, "../../__fixtures__");
 const target = "wordpress-and-node-app";
 const sourceDir = `${FL.TEMPLATES_DIR}/${target}`;
 
-let filesList,
-  DIR_ID,
-  DIR,
-  fakeContent,
-  fakeFileRelativePath,
-  originalFilesList,
-  randomFilePath,
-  targetFilesList;
+let DIR_ID, DIR, fakeContent, fakeFileRelativePath, originalFilesList;
 
 afterAll(async () => {
   let filesAndDirsToRemove = [];
@@ -42,18 +37,14 @@ describe("Initialise", () => {
     expect(filesList.length).toBeGreaterThan(0);
   });
 
+  describe("when a fastline config file doens't exist", () => {
+    it.skip("throws a error", () => {});
+  });
+
   describe("when there is no setup", () => {
     beforeEach(async () => {
       DIR_ID = uuid();
       DIR = path.resolve(DIR_BASE, `initialise-${DIR_ID}`);
-      await fse
-        .readdir(sourceDir, { withFileTypes: true })
-        .then(files => {
-          filesList = files;
-        })
-        .catch(err => {
-          console.error(err);
-        });
       await setup(DIR, target);
     });
 
@@ -68,6 +59,8 @@ describe("Initialise", () => {
       });
       expect(targetFilesList).toEqual(originalFilesList);
     });
+
+    it.skip("replaces all occurrences found on the fastline config file", () => {});
   });
 
   describe("when a setup already exists", () => {
@@ -90,26 +83,6 @@ describe("Initialise", () => {
       fakeExistingFile.end();
 
       await setup(DIR, target);
-      // const root = directoryTree(sourceDir);
-      // // create an alias for later.
-      // targetTemplateRoot = root;
-      // if (root === null) {
-      //   throw new Error(
-      //     `The provided sourceDir doesn't exists. Please check the following path: ${sourceDir}`
-      //   );
-      // } else if (root.type === "directory") {
-      //   const result = new findFileIfExists(root);
-      //   if (typeof result.file === "undefined") {
-      //     throw new Error(
-      //       `No file is present in the following directory: ${sourceDir}`
-      //     );
-      //   }
-      //   randomFilePath = result.file.path.replace(`${FL.TEMPLATES_DIR}/`, "");
-      // } else {
-      //   // Directory tree is actually just a file
-      //   randomFilePath = root.path.replace(`${FL.TEMPLATES_DIR}/`, "");
-      // }
-      // console.log(randomFilePath);
     });
 
     it("doesn't copy the existing templates", async () => {
@@ -121,108 +94,3 @@ describe("Initialise", () => {
     });
   });
 });
-
-/**
- * FIND FILE IF EXISTS
- *
- * It makes use of the directory-tree package.
- * @see https://www.npmjs.com/package/directory-tree
- *
- * WARNING: This can be replaced by walkdir package.
- * @see https://www.npmjs.com/package/walkdir
- *
- * @param dir {Object} A directory-tree object.
- *
- * @example
- *
- *   const result = new findFileIfExists(root);
- *   console.log(result.file);
- *
- *   // Returns an object with the directory-tree structure of the first file found:
- *   //
- *   // {
- *   //   "path": "/photos/summer/june/windsurf.jpg",
- *   //   "name": "windsurf.jpg",
- *   //   "size": 400,
- *   //   "type": "file",
- *   //   "extension": ".jpg"
- *   // }
- **/
-class findFileIfExists {
-  constructor(dir) {
-    this.iterateThroughDir(dir);
-  }
-
-  checkFiles(dir) {
-    const files = dir.children.filter(child => child.type === "file");
-    if (files.length > 0) {
-      this.file = files[0];
-    }
-  }
-
-  iterateThroughDir(originalDir) {
-    this.checkFiles(originalDir);
-    const dirs = originalDir.children.filter(
-      child => child.type === "directory"
-    );
-    // If there are dirs.
-    if (dirs) {
-      // Check every dir in the current level
-      this.iterateOnSameLevel(dirs);
-    }
-  }
-
-  iterateOnSameLevel(dirs) {
-    while (typeof this.file === "undefined" && dirs.children > 0) {
-      let dir = dirs.pop();
-      this.checkFiles(dir);
-      if (typeof this.file === "undefined") {
-        let childrenDirs = dir.children.filter(
-          child => child.type === "directory"
-        );
-        // If there are children dirs.
-        if (childrenDirs) {
-          this.iterateOneLevelDeeper(childrenDirs);
-        }
-      }
-    }
-  }
-
-  iterateOneLevelDeeper(childrenDirs) {
-    while (typeof this.file === "undefined" && childrenDirs.children > 0) {
-      let dir = childrenDirs.pop();
-      this.iterateThroughDir(dir);
-    }
-  }
-}
-
-/**
- * WALK
- *
- * @param dir {String} Directory to walk through
- * @param replacePath {Object} See below
- *                             {
- *                               source: {String} // Part of path to replace
- *                               with: {String} // String you want to use
- *                             }
- *
- *
- **/
-async function walk(dir, replacePath = {}) {
-  let files = await fse.readdir(dir);
-  files = await Promise.all(
-    files.map(async file => {
-      const filePath = path.join(dir, file);
-      const stats = await fse.stat(filePath);
-      if (stats.isDirectory()) {
-        return walk(filePath, replacePath);
-      } else if (stats.isFile()) {
-        let sourceValue = replacePath.source || "";
-        let withValue = replacePath.with || "";
-        return filePath.replace(sourceValue, withValue);
-      }
-    })
-  );
-
-  return files.reduce((all, folderContents) => all.concat(folderContents), []);
-}
--- src/utils/common-functions.js
+++ src/utils/common-functions.js
@@ -1,3 +1,6 @@
+import fse from "fs-extra";
+import path from "path";
+
 /**
  * GET RANDOM INT
  *
@@ -33,4 +36,35 @@ export async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
   }
+}
+
+/**
+ * WALK
+ *
+ * @param dir {String} Directory to walk through
+ * @param replacePath {Object} See below
+ *                             {
+ *                               source: {String} // Part of path to replace
+ *                               with: {String} // String you want to use
+ *                             }
+ *
+ * @return {Array} Array of the paths of all files found
+ **/
+export async function walk(dir, replacePath = {}) {
+  let files = await fse.readdir(dir);
+  files = await Promise.all(
+    files.map(async file => {
+      const filePath = path.join(dir, file);
+      const stats = await fse.stat(filePath);
+      if (stats.isDirectory()) {
+        return walk(filePath, replacePath);
+      } else if (stats.isFile()) {
+        let sourceValue = replacePath.source || "";
+        let withValue = replacePath.with || "";
+        return filePath.replace(sourceValue, withValue);
+      }
+    })
+  );
+
+  return files.reduce((all, folderContents) => all.concat(folderContents), []);
 }
