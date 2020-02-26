ef36f179ac7bc3a4544f10fa4178a95e21579d30
a-barbieri <info@alessandrobarbieri.net>
2020-02-26 16:18:56 +0100

    fix: BASE_DIR not pointing to the right path

--- src/config/init.js
+++ src/config/init.js
@@ -17,18 +17,52 @@ export class FL {
    *   // '/path/to/package/templates'
    **/
   static get TEMPLATES_DIR() {
-    return path.resolve(__dirname, "../../templates");
+    return path.resolve(this.BASE_DIR, "./templates");
   }
+
   /**
    * BASE_DIR constant
    *
-   * @return {String} The absolute path of the directory in which package json is in.
+   * @return {String} The absolute path of Fastline root directory.
    * @example
    *   console.log(FL.TEMPLATES_DIR);
    *   // '/path/to/package/templates'
    **/
   static get BASE_DIR() {
-    return path.resolve(__dirname, "../../");
+    if (__dirname.includes(`/${this.MODULE_NAME}/`)) {
+      return path.join(__dirname.split(this.MODULE_NAME)[0], this.MODULE_NAME);
+    } else {
+      throw new Error(
+        `Impossible to setup BASE_DIR. Fastline module must be either installed inside \`node_modules\` directory or clone inside a directory named \`f${this.MODULE_NAME}\`.`
+      );
+    }
+  }
+
+  /**
+   * NODE_MODULES_DIR constant
+   *
+   * @return {String|null} The absolute path of the `node_modules` folder in which
+   *                      the module is contained.
+   * @example
+   *   console.log(FL.NODE_MODULES_DIR);
+   *   // `/path/to/node_modules`
+   **/
+  static get NODE_MODULES_DIR() {
+    if (__dirname.includes("node_modules")) {
+      // https://stackoverflow.com/questions/60400848/how-to-get-apps-node-modules-absolute-path-from-my-npm-package
+      return path.join(__dirname.split("node_modules")[0], "node_modules");
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * MODULE_NAME constant
+   *
+   * @return {String} The name the module should have. I say should because it's hardcoded.
+   **/
+  static get MODULE_NAME() {
+    return "fastline";
   }
 }
 
