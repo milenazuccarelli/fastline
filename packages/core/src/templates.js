f52ac7cdade0d70ac88732873376b2e388371b47
milenazuccarelli <milena.zuccarelli@gmail.com>
2020-02-28 11:12:09 +0100

    change: variables in template objects

--- packages/core/src/templates.js
+++ packages/core/src/templates.js
@@ -1,15 +1,16 @@
 // TODO: move this in each separate template
 export const templates = {
   "wordpress-and-node-app": {
-    repositoryName: "repo",
-    environment: "staging",
-    WORDPRESS_API_URL: "%WORDPRESS_API_URL%",
-    MYSQL_DATABASE: "%%MYSQL_DATABASE"
+    PROJECT_NAME: "%%PROJECT_NAME%%",
+    REPOSITORY_NAME: "%%PROJECT_NAME%%",
+    WORDPRESS_API_URL: "%%WORDPRESS_API_URL%%",
+    MYSQL_DATABASE: "%%MYSQL_DATABASE%%"
   },
   "wordpress-and-traefik": {
-    repositoryName: "repo",
-    environment: "staging",
-    WORDPRESS_API_URL: "%WORDPRESS_API_URL%",
-    MYSQL_DATABASE: "%%MYSQL_DATABASE"
+    PROJECT_NAME: "%%PROJECT_NAME%%",
+    REPOSITORY_NAME: "%%PROJECT_NAME%%",
+    MYSQL_ROOT_PASSWORD: "%%MYSQL_ROOT_PASSWORD%%",
+    MYSQL_USER: "%%MYSQL_USER%%",
+    MYSQL_PASSWORD: "%%MYSQL_PASSWORD%%"
   }
 };
