eda2623105e6549ceafce15e2d61164ed289b309
milenazuccarelli <milena.zuccarelli@gmail.com>
2020-03-02 09:56:20 +0100

    chore: move templates configuration

--- packages/core/src/main.js
+++ packages/core/src/main.js
@@ -1,10 +1,10 @@
 "use strict";
 const inquirer = require("inquirer");
+import fse from "fs-extra";
 
 import { FL } from "./config/init";
 import { setup } from "./lib/initalise";
 import { walkDir } from "./utils/common-functions";
-import { templates } from "./templates.js";
 
 export default async () => {
   const firstAnswers = await getFirstAnswers();
@@ -18,14 +18,22 @@ export default async () => {
   // get the items that need to be replaced
   // depending on the template type
   // TODO: template object should be taken from each template package
-  const template = templates[templateName];
+
+  const template = await fse.readFile(
+    `${firstAnswers["templates"]}/${templateName}/template.json`
+  );
+
+  const templateJSON = JSON.parse(template);
+
+  console.log(templateJSON);
+
   let replacements = {};
 
   // loop through the keys for the specified template
-  for (let key in template) {
-    // template[key] is the field in the object i.e. %%PROXY%%
+  for (let key in templateJSON) {
+    // templateJSON[key] is the field in the object i.e. %%PROXY%%
     // prompt the user for the desired replacement
-    const { target } = await getVariableReplacement(key, template[key]);
+    const { target } = await getVariableReplacement(key, templateJSON[key]);
     // store the replacement in a new object, with the same
     // key as the original, so the objects have a similar structure
     replacements[key] = target;
@@ -40,11 +48,11 @@ export default async () => {
 
   // for demo purposes
   // loop through the template again
-  for (let key in template) {
+  for (let key in templateJSON) {
     keys.push(key);
 
     // get the pattern to be replaced
-    let pattern = template[key];
+    let pattern = templateJSON[key];
     regexPatterns.push(pattern);
 
     // get the replacement
