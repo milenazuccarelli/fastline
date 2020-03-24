144e06f08eb1287a0d9d565e371eeb61b833692e
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-03-24 15:44:24 +0100

    chore: setup rails skeleton with template

---
+++ templates/binda-cms/config/web/template.rb
@@ -0,0 +1,35 @@
+# Add gems
+gem "binda"
+
+# Install dependencies
+run "bundle install"
+
+# Remove git repository
+run "rm -rf .git"
+
+# Override database.yml
+file 'config/database.yml', <<-CODE
+   default: &default
+   adapter: postgresql
+   encoding: unicode
+   pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
+   host: <%= ENV['POSTGRES_HOST'] %>
+   username: postgres
+
+   development:
+   <<: *default
+   database: myapp_development
+   password: <%= ENV['POSTGRES_PASSWORD'] %>
+
+   test:
+   <<: *default
+   database: myapp_test
+   password: <%= ENV['POSTGRES_PASSWORD'] %>
+
+   production:
+   <<: *default
+   database: myapp_production
+   password: <%= ENV['POSTGRES_PASSWORD'] %>
+CODE
+
+
