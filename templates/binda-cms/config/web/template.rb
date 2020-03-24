139fa5c80f81ce0fd70da5fe3cf7400a75ef7a46
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-03-24 16:03:30 +0100

    chore: fix indentation of database.yml

--- templates/binda-cms/config/web/template.rb
+++ templates/binda-cms/config/web/template.rb
@@ -10,26 +10,26 @@ run "rm -rf .git"
 # Override database.yml
 file 'config/database.yml', <<-CODE
    default: &default
-   adapter: postgresql
-   encoding: unicode
-   pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
-   host: <%= ENV['POSTGRES_HOST'] %>
-   username: postgres
+      adapter: postgresql
+      encoding: unicode
+      pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
+      host: <%= ENV['POSTGRES_HOST'] %>
+      username: postgres
 
    development:
-   <<: *default
-   database: myapp_development
-   password: <%= ENV['POSTGRES_PASSWORD'] %>
+      <<: *default
+      database: myapp_development
+      password: <%= ENV['POSTGRES_PASSWORD'] %>
 
    test:
-   <<: *default
-   database: myapp_test
-   password: <%= ENV['POSTGRES_PASSWORD'] %>
+      <<: *default
+      database: myapp_test
+      password: <%= ENV['POSTGRES_PASSWORD'] %>
 
    production:
-   <<: *default
-   database: myapp_production
-   password: <%= ENV['POSTGRES_PASSWORD'] %>
+      <<: *default
+      database: myapp_production
+      password: <%= ENV['POSTGRES_PASSWORD'] %>
 CODE
 
 
