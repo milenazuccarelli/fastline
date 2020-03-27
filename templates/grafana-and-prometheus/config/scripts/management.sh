d740e4699b9ef6d238df3a48b22123174d53e3e8
milenazuccarelli <milena.zuccarelli@gmail.com>
2020-03-27 10:03:18 +0000

    chore: remove current templates

--- templates/grafana-and-prometheus/config/scripts/management.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker stack deploy -c ../management/traefik/docker-compose.yml management_traefik
-docker stack deploy -c ../management/swarmpit/docker-compose.yml management_swarmpit
-docker stack deploy -c ../management/monitoring/docker-compose.yml management_monitoring
